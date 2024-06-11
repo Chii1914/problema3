d2b59e9c6b53d4dc639596af253f6d3dfca6d4c7
Chii1914 <maximiliano.aguirre@alumnos.uv.cl>
2024-06-11 01:16:11 -0400

    Refactor app.js to remove commented out code for compradorRoutes

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const sequelize = require('./config/database');
 
 // Importar rutas
 const vendedorRoutes = require('./routes/vendedorRoutes');
-const compradorRoutes = require('./routes/compradorRoutes');
+//const compradorRoutes = require('./routes/compradorRoutes');
 const productoRoutes = require('./routes/productoRoutes');
 
 app.use(express.json());
 
 // Usar rutas
 app.use('/api/vendedores', vendedorRoutes);
-app.use('/api/compradores', compradorRoutes);
+//app.use('/api/compradores', compradorRoutes);
 app.use('/api/productos', productoRoutes);
 
 app.set('view engine', 'ejs');
